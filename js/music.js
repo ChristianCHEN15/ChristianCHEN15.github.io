560497176934a0269a9b5f26570bb3ed5cdd0ca0
christinmin <2392684064qq@gmail.com>
2024-09-03 22:46:27 +0800

    Site updated: 2024-09-03 22:46:26

--- js/music.js
+++ js/music.js
@@ -4,8 +4,8 @@ const ap = new APlayer({
 	autoplay: true, //自动播放
     audio: [
 	{
-        name: "浴室",
-        artist: 'deca joins',
+        name: "Mine (Illenium Remix)",
+        artist: 'ILLENIUM / Phoebe Ryan',
         url: 'https://m701.music.126.net/20240903162132/3bb2ba98aae34183f83b65658727db5b/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/35836669993/2dee/7025/5240/4e0f8bb1126697bfe0aae9b088b446de.m4a',
         cover: 'https://p1.music.126.net/byjfkEIOWI_RmxSKEWTPyw==/18910500486297525.jpg?param=200y200',	
     },
