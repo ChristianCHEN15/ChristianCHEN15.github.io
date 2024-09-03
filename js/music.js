02247616945ed1453a681152a2b579d45c0ebe16
christinmin <2392684064qq@gmail.com>
2024-09-03 16:01:05 +0800

    Site updated: 2024-09-03 16:01:04

---
+++ js/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+	autoplay: true, //自动播放
+    audio: [
+	{
+        name: "浴室",
+        artist: 'deca joins',
+        url: 'https://m701.music.126.net/20240903162132/3bb2ba98aae34183f83b65658727db5b/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/35836669993/2dee/7025/5240/4e0f8bb1126697bfe0aae9b088b446de.m4a',
+        cover: 'https://p1.music.126.net/byjfkEIOWI_RmxSKEWTPyw==/18910500486297525.jpg?param=200y200',	
+    },
+	]
+});
